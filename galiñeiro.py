4684377a4b833ea31d270d1cc7f4e8d287c02e55
Jorge Lopez Seijas <jorginho@riseup.net>
2016-11-24 21:07:41 +0100

    Modificado codigo para axustalo as tabulacions

--- galiñeiro.py
+++ galiñeiro.py
@@ -47,7 +47,7 @@ CandadoPrograma = threading.Lock()	# SEMAFORO
 
 app = Flask(__name__) # Acceso rapido de funcions
 
-	# WEBCAM
+# WEBCAM
 @app.route('/galinheiro/snapshot', methods=['GET']) #URL FUNCION
 def Camara():
     cameraProcess = subprocess.Popen("fswebcam -d /dev/video0 -i gspca_zc3xx -r 320x232 -S 10 --jpeg 80 --no-banner --save /var/www/html/snapshot.jpg".split());
@@ -67,9 +67,7 @@ def Camara():
 
     return jsonify({"boolean":1})
 
-    # ENCENDER INCANDESCENTE
-
-
+# ENCENDER INCANDESCENTE
 def Encender_Incandescente(dispositivo):
     global Incandescente
     global IncandescenteMovil
@@ -81,198 +79,197 @@ def Encender_Incandescente(dispositivo):
 
 @app.route('/galinheiro/encender_incandescente/', methods=['GET'])
 def Encender_Incandescente_Movil():
-	global Incandescente
+    global Incandescente
     global IncandescenteMovil
 
-	if Incandescente == 0:
-		Encender_Incandescente(1)
+    if Incandescente == 0:
+    Encender_Incandescente(1)
 
-	return jsonify({"incandescente": IncandescenteMovil})
+    return jsonify({"incandescente": IncandescenteMovil})
 
-	# APAGAR INCANDESCENTE
-
+# APAGAR INCANDESCENTE
 def Apagar_Incandescente(dispositivo):
     global Incandescente
     global IncandescenteMovil
-    
-	GPIO.output(4, True) # Encender luz
+
+    GPIO.output(4, True) # Encender luz
     IncandescenteMovil = 0
 
-	print "Apagar", dispositivo
+    print "Apagar", dispositivo
 
 
 @app.route('/galinheiro/apagar_incandescente/', methods=['GET'])
 def Apagar_Incandescente_Movil():
-	global Incandescente
+    global Incandescente
     global IncandescenteMovil
 
-	if Incandescente == 0:
-		Apagar_Incandescente(1)
+    if Incandescente == 0:
+        Apagar_Incandescente(1)
 
     return jsonify({"incandescente": IncandescenteMovil})
 
-	# ABRIR PORTA
+# ABRIR PORTA
 def Abrir_Porta(dispositivo):
-	global porta
+    global porta
 
-	CandadoAbrirCerrarPorta.acquire()
-	porta = 1
-	print "Abrir", dispositivo
-	GPIO.output(15, False) # Alimentar Fuente
-	time.sleep(1)
-	GPIO.output(24, False) # Alimentamos Motor (Sentido 2)
-	time.sleep(22)
-	GPIO.output(15, True)	# Desconectamos Fuente
-	GPIO.output(24, True)	# Desconectamos Motor
-	CandadoAbrirCerrarPorta.release()
+    CandadoAbrirCerrarPorta.acquire()
+    porta = 1
+    print "Abrir", dispositivo
+    GPIO.output(15, False) # Alimentar Fuente
+    time.sleep(1)
+    GPIO.output(24, False) # Alimentamos Motor (Sentido 2)
+    time.sleep(22)
+    GPIO.output(15, True)	# Desconectamos Fuente
+    GPIO.output(24, True)	# Desconectamos Motor
+    CandadoAbrirCerrarPorta.release()
 
 @app.route('/galinheiro/abrir_porta/', methods=['GET'])
 def Abrir_Porta_Movil():
-	global Pulsador
-	global manAuto
-	global porta
+    global Pulsador
+    global manAuto
+    global porta
 
-	if Pulsador == 0 and manAuto == 1:
-		if porta == 0:
-			Abrir_Porta(1)
-		return jsonify({"codigo": True})
-	else:
-		return jsonify({"codigo": False})
+    if Pulsador == 0 and manAuto == 1:
+        if porta == 0:
+            Abrir_Porta(1)
+        return jsonify({"codigo": True})
+    else:
+        return jsonify({"codigo": False})
 
 def Cerrar_Porta(dispositivo):
-	global porta
+    global porta
 
-	CandadoAbrirCerrarPorta.acquire()
-	porta = 0
-	print "Cerrar", dispositivo
-	GPIO.output(15, False) # Alimentar Fuente
-	time.sleep(1)
-	GPIO.output(18, False) # Alimentamos - 0 V CC
-	time.sleep(1)
-	GPIO.output(23, False)	# Alimentamos Motor (Sentido 1)
-	time.sleep(22)
-	GPIO.output(15, True)	# Desconectamos Fuente
-	GPIO.output(23, True)	# Desconectamos Motor
-	time.sleep(1)
-	GPIO.output(18, True)	# Desconectamos -0V CC
-	CandadoAbrirCerrarPorta.release()
+    CandadoAbrirCerrarPorta.acquire()
+    porta = 0
+    print "Cerrar", dispositivo
+    GPIO.output(15, False) # Alimentar Fuente
+    time.sleep(1)
+    GPIO.output(18, False) # Alimentamos - 0 V CC
+    time.sleep(1)
+    GPIO.output(23, False)	# Alimentamos Motor (Sentido 1)
+    time.sleep(22)
+    GPIO.output(15, True)	# Desconectamos Fuente
+    GPIO.output(23, True)	# Desconectamos Motor
+    time.sleep(1)
+    GPIO.output(18, True)	# Desconectamos -0V CC
+    CandadoAbrirCerrarPorta.release()
 
 @app.route('/galinheiro/cerrar_porta/', methods=['GET'])
 def Cerrar_Porta_Movil():
-	global Pulsador
-	global manAuto
-	global porta
+    global Pulsador
+    global manAuto
+    global porta
 
-	if Pulsador == 0 and manAuto == 1:
-		if porta == 1:
-			Cerrar_Porta(1)
-		return jsonify({"codigo": True})
-	else:
-		return jsonify({"codigo": False})
+    if Pulsador == 0 and manAuto == 1:
+        if porta == 1:
+            Cerrar_Porta(1)
+            return jsonify({"codigo": True})
+    else:
+        return jsonify({"codigo": False})
 
 def Encender_Luz_Estado_Pulsador():
-	GPIO.output(15, False)
-	time.sleep(1)
-	GPIO.output(25, False)
+    GPIO.output(15, False)
+    time.sleep(1)
+    GPIO.output(25, False)
 
 def Apagar_Luz_Estado_Pulsador():
-	GPIO.output(15, True)
-	GPIO.output(25, True)
+    GPIO.output(15, True)
+    GPIO.output(25, True)
 
 @app.route('/galinheiro/parametros', methods=['GET'])
 def Parametros():
-	global porta
-	global manAuto
-	global IncandescenteMovil
+    global porta
+    global manAuto
+    global IncandescenteMovil
 
-	return jsonify({"porta": porta, "manAuto": manAuto, "incandescente": IncandescenteMovil})
+    return jsonify({"porta": porta, "manAuto": manAuto, "incandescente": IncandescenteMovil})
 
 @app.route('/galinheiro/automatico_manual/<int:estado>', methods=['GET'])
 def Automatico_Manual(estado):
-	global Pulsador
-	global manAuto
+    global Pulsador
+    global manAuto
 
-	if Pulsador == 0:
-		manAuto = estado
+    if Pulsador == 0:
+        manAuto = estado
 
-	return jsonify({"manAuto": manAuto})
+    return jsonify({"manAuto": manAuto})
 
 
 def fioManAuto():
-	global manAuto
+    global manAuto
 
-	while True:
-		tempo = 0
-		while manAuto == 0:
-			time.sleep(10)
+    while True:
+        tempo = 0
+        while manAuto == 0:
+            time.sleep(10)
 
-		while manAuto == 1 and tempo < 600:
-			time.sleep(1)
-			tempo += 1
+        while manAuto == 1 and tempo < 600:
+            time.sleep(1)
+            tempo += 1
 
-		manAuto = 0
+        manAuto = 0
 
 @app.route("/")
 def hello():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/galinheiro/', methods=['GET'])
 def Programa():
-	CandadoPrograma.acquire()
-	global Pulsador
-	global manAuto
-	global Incandescente
-	global cerreManual
+    CandadoPrograma.acquire()
+    global Pulsador
+    global manAuto
+    global Incandescente
+    global cerreManual
 
-	fioEstadoManAuto = threading.Thread(target=fioManAuto)
-	fioEstadoManAuto.start()
+    fioEstadoManAuto = threading.Thread(target=fioManAuto)
+    fioEstadoManAuto.start()
 
-	Cerrar_Porta(0) # Cerramos portal como primeira instrucción para determinar a posición do portal
+    Cerrar_Porta(0) # Cerramos portal como primeira instrucción para determinar a posición do portal
 
-	while True:		# Bucle de funcionamento do Programa
-					# CICLO MANUAL
-		"""
-		if GPIO.input(20) == False and Pulsador == 0:
-			print "Manual 1"
-			Pulsador = 1
-			manAuto = 0 #Prioriza o pulsador cuadro sobre mobil
-			Encender_Luz_Estado_Pulsador()
-			if porta == 0:
-				Abrir_Porta(0)
+    while True:		# Bucle de funcionamento do Programa
+    	# CICLO MANUAL
+        """
+        if GPIO.input(20) == False and Pulsador == 0:
+            print "Manual 1"
+            Pulsador = 1
+            manAuto = 0 #Prioriza o pulsador cuadro sobre mobil
+            Encender_Luz_Estado_Pulsador()
+            if porta == 0:
+                Abrir_Porta(0)
 
-		if GPIO.input(20) == False and Pulsador == 1:
-			print "Manual 2"
-			Pulsador = 2
-			Incandescente = 1
-			Encender_Incandescente(0)
-			while GPIO.input(20) == False:
-				continue
+        if GPIO.input(20) == False and Pulsador == 1:
+            print "Manual 2"
+            Pulsador = 2
+            Incandescente = 1
+            Encender_Incandescente(0)
+            while GPIO.input(20) == False:
+                continue
 
-		if GPIO.input(20) == False and Pulsador == 2:
-			print "Automatico"
-			Apagar_Incandescente(0)
-			Incandescente = 0
-			Apagar_Luz_Estado_Pulsador()
-			Pulsador = 0
-			while GPIO.input(20) == False:
-				continue
-		"""
-		if Pulsador == 0 and manAuto == 0: # CICLO AUTOMATICO
-			if GPIO.input(16) == False and porta == 1: # Cerrar Porta Noite
-				Incandescente = 1
-				if cerreManual == 1:
-					Encender_Incandescente(0)
-					time.sleep(1200)
-					cerreManual = 0
-				Cerrar_Porta(0)
-				Apagar_Incandescente(0)
-				Incandescente = 0
+        if GPIO.input(20) == False and Pulsador == 2:
+            print "Automatico"
+            Apagar_Incandescente(0)
+            Incandescente = 0
+            Apagar_Luz_Estado_Pulsador()
+            Pulsador = 0
+            while GPIO.input(20) == False:
+                continue
+        """
+    if Pulsador == 0 and manAuto == 0: # CICLO AUTOMATICO
+        if GPIO.input(16) == False and porta == 1: # Cerrar Porta Noite
+            Incandescente = 1
+            if cerreManual == 1:
+                Encender_Incandescente(0)
+                time.sleep(1200)
+                cerreManual = 0
+            Cerrar_Porta(0)
+            Apagar_Incandescente(0)
+            Incandescente = 0
 
-			if GPIO.input(12) == False and porta == 0: # Abrir Porta Día
-				Abrir_Porta(0)
-				cerreManual = 1
+    if GPIO.input(12) == False and porta == 0: # Abrir Porta Día
+        Abrir_Porta(0)
+        cerreManual = 1
 
-	Candado2.release()
+    Candado2.release()
 
 if __name__ == '__main__':					# Indica si se esta cargando desde un arquivo principal ou é un modulos esterno
-   	app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True)
